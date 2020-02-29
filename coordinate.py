1da24c413a3c385d5dbc9eddfde4a718fcf5b102
furquan5 <60378819+furquan5@users.noreply.github.com>
2020-02-29 11:03:29 +0530

    Add files via upload

---
+++ coordinate.py
@@ -0,0 +1,10 @@
+class Coordinate:
+
+       def __init__(self,x,y):
+              self.num1=x
+              self.num2=y
+
+       def __str__(self):
+              return '{},{}'.format(self.num1,self.num2)
+
+       
